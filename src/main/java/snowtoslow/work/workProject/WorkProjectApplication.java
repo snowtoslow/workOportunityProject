38440989144747c31a42d4f4c089e91cf462f1b0
snowtoslow <snowtoslow@mail.ru>
2019-11-03 01:40:55 +0200

    Delete test stuff;

--- src/main/java/snowtoslow/work/workProject/WorkProjectApplication.java
+++ src/main/java/snowtoslow/work/workProject/WorkProjectApplication.java
@@ -1,17 +1,12 @@
 package snowtoslow.work.workProject;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorkProjectApplication {
 
 	public static void main(String[] args) {
-
-
-
-
-
-
-
+		SpringApplication.run(WorkProjectApplication.class, args);
 	}
 }
