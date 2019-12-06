a379c0dd92987466f96b91f742827b95bb7dc6f3
mrdantutunaru <danulutu@mail.ru>
2019-12-06 16:45:23 +0200

    add user ID and add by whom its created;

--- src/main/java/snowtoslow/work/workProject/repository/CommentRepository.java
+++ src/main/java/snowtoslow/work/workProject/repository/CommentRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import snowtoslow.work.workProject.models.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
 }
--- src/main/java/snowtoslow/work/workProject/repository/PostRepository.java
+++ src/main/java/snowtoslow/work/workProject/repository/PostRepository.java
@@ -3,8 +3,9 @@ package snowtoslow.work.workProject.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import snowtoslow.work.workProject.models.Post;
 
-public interface PostRepository extends JpaRepository<Post, Integer> {
 
+
+public interface PostRepository extends JpaRepository<Post, Integer> {
 
 
 }
